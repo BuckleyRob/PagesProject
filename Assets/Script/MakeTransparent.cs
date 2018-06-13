3fc30876aa682456ab6007273ce242425a589748
Robert Buckley <RobertBuckley@gmail.com>
2018-06-13 00:07:52 -0700

    add models and stuff

---
+++ Assets/Script/MakeTransparent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeTransparent : MonoBehaviour {
+	public LayerMask layer;
+	private void OnTriggerEnter(Collider other) {
+		if(other.gameObject.layer == layer.value)
+			return;
+		Debug.Log("Got Entyer");
+		Renderer otherR = other.gameObject.GetComponent<Renderer>();
+		if(otherR != null){
+			Color c;
+			foreach(Material m in otherR.materials){
+				c = m.color;
+				c.a = 0f;
+				m.color = c;
+			}
+		}
+	}
+	void OnTriggerExit(Collider other){
+		if((other.gameObject.layer & layer.value) == 0)
+			return;
+		Debug.Log("Got Exit");
+		Renderer otherR = other.gameObject.GetComponent<Renderer>();
+		if(otherR != null){
+			Color c;
+			foreach(Material m in otherR.materials){
+				c = m.color;
+				c.a = 254f;
+				m.color = c;
+			}
+		}
+	}
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+		movement = Quaternion.Euler(0f,-45f,0f) * movement;
+		rb.AddForce(movement * speed);
+	}
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Pick Up")) {
+			other.gameObject.SetActive(false);
+			count += 1;
+			SetCountText();
+		}
+	}
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString();
+		if(count >= 12)
+			winText.text = "You Win!";
+	}
+}
